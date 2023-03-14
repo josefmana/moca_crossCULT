e9d9be048a49c5763ca54eab74a832e3aa14dbaf
josefmana <josef.mana@protonmail.com>
2023-03-14 23:32:18 +0100

    data reading and cleaning ...

--- moca_crossCULT_stat_modelling.R
+++ moca_crossCULT_stat_modelling.R
@@ -29,6 +29,9 @@ d <- list(
   exp = read.csv( "data/moca_crossCULT_data_exp.csv", sep = ";" )
 )
 
+# for further analyses keep only paired subjects (i.e., those ctrl subjects that have been paired with an exp subject)
+d_nanok <- list( incl = d$dem[ d$dem$exp.id != "", ], excl = d$dem[ d$dem$exp.id == "", ] )
+
 # add sum scores wherever missing
 for ( i in c("ctr","exp") ) {
   for ( j in c("clock","naming","abstraction","free_recall") ) d[[i]][,j] <- rowSums( d[[i]][ , grepl( paste0(j,"_"), colnames(d[[i]]) ) ] )
@@ -36,5 +39,25 @@ for ( i in c("ctr","exp") ) {
 
 # keep only the items list in struct
 df <- d$ctr[ , colnames(d$ctr) %in% c("id",struct$item) ] %>%
-  mutate( grp = "ctrl" ) %>%
-  rbind.data.frame( d$exp[ , colnames(d$exp) %in% c("id",struct$item) ] %>% mutate( grp = "exp" ) )
+  # add an index of the group and flag subjects to be included
+  add_column( grp = "ctrl", .after = 1 ) %>%
+  mutate( included = ifelse( id %in% d$dem[ d$dem$exp.id != "", "ctr.id"], 1, 0 ), .after = 2 ) %>%
+  # bind the new experimental subjects below the old normative control ones
+  rbind.data.frame(
+    d$exp[ , colnames(d$exp) %in% c("id",struct$item) ] %>%
+      add_column( grp = "exp", .after = 1 ) %>%
+      mutate( included = ifelse( id %in% d$dem[ d$dem$exp.id != "", "exp.id"], 1, 0 ), .after = 2 )
+  ) %>%
+  # add columns for demographic variables
+  add_column( age_y = NA, .after = 2 ) %>%
+  add_column( edu_y = NA, .after = 3 ) %>%
+  add_column( sex = NA, .after = 4 )
+
+# add demographic variables via loops
+for ( i in df$id ) {
+  
+  if ( i %in% d$dem$ctr.id ) df[ df$id == i , c("age_y","edu_y","sex") ] <- d$dem[ d$dem$ctr.id == i, paste0( "ctr.",c("age_y","edu_y","sex") ) ]
+  if ( i %in% d$dem$exp.id ) df[ df$id == i , c("age_y","edu_y","sex") ] <- d$dem[ d$dem$exp.id == i, paste0( "exp.",c("age_y","edu_y","sex") ) ]
+  if ( i %in% d$dem$excl.id ) df[ df$id == i , c("age_y","edu_y","sex") ] <- d$dem[ d$dem$excl.id == i, paste0( "excl.",c("age_y","edu_y","sex") ) ]
+
+}
